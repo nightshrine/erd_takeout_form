f921c5c65d3ee4c6ad790dd0054c5234c9853604
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2024-08-10 13:39:54 +0900

    #4 Reactの型を修正

--- frontend/src/definitions/ITransactionResponse.ts
+++ frontend/src/definitions/ITransactionResponse.ts
@@ -1,10 +1,10 @@
-import { ICustomer } from "./orderTransaction/ICustomerTransaction";
-import { IOrder } from "./orderTransaction/IOrderTransaction";
+import { ICustomerTransaction } from "./orderTransaction/ICustomerTransaction";
+import { IOrderTransaction } from "./orderTransaction/IOrderTransaction";
 
 /** トランザクションレスポンス */
 export interface ITransactionResponse {
   /** 顧客データ */
-  customer: ICustomer[];
+  customer: ICustomerTransaction[];
   /** 注文データ */
-  order: IOrder[];
+  order: IOrderTransaction[];
 }
