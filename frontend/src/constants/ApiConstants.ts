ee5ff96ae110be386929af73e81713cbb9a508cf
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2024-08-11 16:23:41 +0900

    #6 frontend BASE_API_URL修正

--- frontend/src/constants/ApiConstants.ts
+++ frontend/src/constants/ApiConstants.ts
@@ -1,5 +1,6 @@
 // TODO: envに置きたい
-export const BASE_API_URL = "https://erd_takeout_form_backend.workers.dev";
+export const BASE_API_URL =
+  "https://erd_takeout_form_backend.nightshrine.workers.dev";
 // マスタ初期化API
 export const INIT_MASTER_API = "/api/init/master";
 // トランザクションデータ取得API
